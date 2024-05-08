da0eee1d8edeb6e90546b334a1b848fdd618b7ee
Mohamed Khaled <168598643+Mo-Khaledd@users.noreply.github.com>
2024-05-09 00:57:35 +0300

    Update GPS_Read.c

--- GPS/GPS_Read.c
+++ GPS/GPS_Read.c
@@ -20,18 +20,18 @@ $GPRMC,203522.0,A,5109.0262308,N,11401.8407342,W,0.004,133.4,130522,0.0,E,D
 void GPS_Read(){
   
 	char GPS_log[80];     
-  char logName[] = "$GPRMC";
+        char logName[] = "$GPRMC";
 	
 	//Making Sure we recieved the correct Log "$GPRMC"
 	char flag = 1;
 	do{
-		flag =1;
-		for (char i=0; i<7; i++){
-        if(UART_Rx() != logName[i]){
-           flag=0;
-           break;   					
-				}			
-		}
+	   flag =1;
+           for (char i=0; i<7; i++){
+              if(UART_Rx() != logName[i]){
+              flag=0;
+              break;   					
+	      }			
+	   }
 	}while(flag == 0);
 
 	
@@ -41,13 +41,11 @@ void GPS_Read(){
 	
 	//Filling the GPS_log array with characters until reaching character '*'
 	char charRecieved;
-	do{
-		
-		char logFillCounter = 0;
-		charRecieved = UART_Rx();
-		GPS_log[logFillCounter] = charRecieved;
-		logFillCounter++;
-		
+	do{	
+	  char logFillCounter = 0;
+	  charRecieved = UART_Rx();
+	  GPS_log[logFillCounter] = charRecieved;
+	  logFillCounter++;	
 	}while(charRecieved != '*');
 	
 	
@@ -58,9 +56,9 @@ void GPS_Read(){
 	p = strtok(GPS_log, ",");
   
 	do{
-		strcpy(logFormatted[noOfStrings] , p);
-	  p = strtok(NULL , ","); 
-    noOfStrings++;	
+	   strcpy(logFormatted[noOfStrings] , p);
+	   p = strtok(NULL , ","); 
+           noOfStrings++;	
 	}while(p != NULL);
 		
 }
