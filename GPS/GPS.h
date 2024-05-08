5a599ebf918dcff1cbfd70f8935d265b5c596cf0
M47 <107054902+MarwanAhmed2100902@users.noreply.github.com>
2024-05-08 22:13:25 +0300

    Update GPS.h

--- GPS/GPS.h
+++ GPS/GPS.h
@@ -1,4 +1,21 @@
+#ifndef GPS
+#define GPS
+
+#define PI 3.14159265358979323846264338327950288
+#define EARTH_RADIUS 6371000
+
 char logFormated [12][20];
 
 void start_walking(float* trajectory_points);
 void GPS_Read();
+
+
+
+float to_degree(float angle);
+
+float to_rad(float degree);
+
+float TotalDistance(float* array );  // return distance in meters
+
+
+#endif
