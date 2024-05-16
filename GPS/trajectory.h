992b4f8c956e4c5ed25a488095d4d73081e0b5d9
Mohamed-Elahmar <168572440+Mohamed-Elahmar@users.noreply.github.com>
2024-05-16 23:33:05 +0300

    Update trajectory.h

--- GPS/trajectory.h
+++ GPS/trajectory.h
@@ -1,4 +1,15 @@
-char logFormated [12][20];
-
-void start_walking(float* trajectory_points);
+#ifndef trajectory
+#define trajectory
+#define arrayGlopal  (* char)logFormatted
+	
+void UART2_Tx(char Data);
+char UART2_Rx(void);
+void UART0_Tx(char Data);
+char UART0_Rx(void);
+void UART0_Tx_int(int Data);
+void UART2_Tx_int(int Data);
+float start_walking(float* trajectory_points);
 void GPS_Read();
+
+
+#endif
