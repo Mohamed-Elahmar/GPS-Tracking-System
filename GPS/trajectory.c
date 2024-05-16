4b66d6122f48390fabf65f384be134d5c6d58f84
Mohamed Khaled <168598643+Mo-Khaledd@users.noreply.github.com>
2024-05-16 23:41:27 +0300

    Update trajectory.c

--- GPS/trajectory.c
+++ GPS/trajectory.c
@@ -4,9 +4,70 @@
 #include "timer.h"
 #include <String.h>
 #include <stdlib.h>
-
+#include "Initialization.h"
 #include "Switch.h"
 #include "LED.h"
+#include "DistanceFunction.h"
+
+
+int flagx=0;
+float currentDistance;
+float DistanceAccstart ;
+int k=0;
+char rec;
+
+char GPS_log[80];
+char logFormatted [12][20];  // a 2D array of 12 rows, each row represents a substring of GPS_log array 
+
+
+
+/*
+Description:
+A function that reads a complete sentence from the GPS module then it Cuts it into substrings 
+
+Example of complete sentence:
+$GPRMC,203522.0,A,5109.0262308,N,11401.8407342,W,0.004,133.4,130522,0.0,E,D     
+  "around 80 characters"
+
+*/
+
+void GPS_Read(){
+	
+     char logFillCounter = 0;
+     char noOfStrings = 0;
+     char *p;
+     char charRecieved;
+     int i ;
+     int j=0;
+     char logName[] = "$GPRMC,";
+
+	
+    //Making Sure we recieved the correct Log "$GPRMC"
+	do{
+	   while(UART2_Rx() != logName[j]);
+	   j++;	
+	}while(j != 7);
+
+	
+    //Initializing the log array to null
+	strcpy(GPS_log ,"");
+	
+	
+    //Filling the GPS_log array with characters until reaching character '*'
+	do{
+	   charRecieved = UART2_Rx();
+	   GPS_log[logFillCounter] = charRecieved;
+	   logFillCounter++;	
+	}while(charRecieved != '*');
+
+	
+     //Cutting GPS_log array into multiple strings 
+	p = strtok(GPS_log, ",");
+	do{
+	  strcpy(logFormatted[noOfStrings++] , p);
+	  p = strtok(NULL , ","); 	
+	}while(p != NULL);	
+}
 
 
 
