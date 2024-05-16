d10614bcde14bda3b61cfc3841aac2b9ba7ba54e
Mohamed Khaled <168598643+Mo-Khaledd@users.noreply.github.com>
2024-05-16 23:27:50 +0300

    Update GPS_Read.c

--- GPS/GPS_Read.c
+++ GPS/GPS_Read.c
@@ -14,25 +14,23 @@ A function that reads a complete sentence from the GPS module then cuts it into 
 Example of complete sentence:
 $GPRMC,203522.0,A,5109.0262308,N,11401.8407342,W,0.004,133.4,130522,0.0,E,D     
   "around 80 characters"
-
 */
 
 void GPS_Read(){
-  
-	char GPS_log[80];     
-        char logName[] = "$GPRMC";
-	
-	//Making Sure we recieved the correct Log "$GPRMC"
-	char flag = 1;
+        char GPS_log[80];
+	char logName[] = "$GPRMC,";
+	char logFillCounter = 0;
+	char noOfStrings = 0;
+	char *p;
+	char charRecieved;
+	int i ;
+        int j=0;
+	     
+	//Making Sure we recieved the correct Log "$GPRMC,"
 	do{
-	   flag =1;
-           for (char i=0; i<7; i++){
-              if(UART_Rx() != logName[i]){
-              flag=0;
-              break;   					
-	      }			
-	   }
-	}while(flag == 0);
+	    while(UART2_Rx() != logName[j]);
+	    j++;	
+	}while(j != 7);
 
 	
 	//Initializing the log array to null
@@ -40,27 +38,21 @@ void GPS_Read(){
 	
 	
 	//Filling the GPS_log array with characters until reaching character '*'
-	char charRecieved;
-	do{	
-	  char logFillCounter = 0;
-	  charRecieved = UART_Rx();
-	  GPS_log[logFillCounter] = charRecieved;
-	  logFillCounter++;	
+	do{
+            charRecieved = UART2_Rx();
+	    GPS_log[logFillCounter] = charRecieved;
+	    logFillCounter++;	
 	}while(charRecieved != '*');
 	
 	
 	//Cutting GPS_log array into multiple strings 
-	char noOfStrings = 0;
-	char *p;
-	
 	p = strtok(GPS_log, ",");
   
 	do{
-	   strcpy(logFormatted[noOfStrings] , p);
-	   p = strtok(NULL , ","); 
-           noOfStrings++;	
+	   strcpy(logFormatted[noOfStrings++] , p);
+	   p = strtok(NULL , ","); 	
 	}while(p != NULL);
-		
+	
 }
 
 
